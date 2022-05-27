80a795d84e448b1a65647e738335498af583a637
My name <me@example.com>
2022-05-27 16:27:30 +0200

    changed from 3 sensors to 2 sensors

--- ReadyCode/ReadyCode.ino
+++ ReadyCode/ReadyCode.ino
@@ -1,15 +1,15 @@
 //Commenting for a test.
 
-#include <AFMotor.h>  
+#include <AFMotor.h>
 int trigPinM = A2;
-int echoPinM = A3; 
+int echoPinM = A3;
 int trigPinR = A0;
-int echoPinR = A1; 
+int echoPinR = A1;
 
 int trigPinL = A4;
 int echoPinL = A5;
 
-AF_DCMotor motorR (1); 
+AF_DCMotor motorR (1);
 AF_DCMotor motorL (2);
 AF_DCMotor motorR1 (3);
 AF_DCMotor motorL1 (4);
@@ -18,10 +18,10 @@ long duration, distance;
 long dmiddle;
 long dright;
 long dleft;
-int dis = 20;
-int range = 2;
-int range2 = 12;
+int dis = 10;
+int range = 20;
 int vel = 200;
+int del = 300;
 void setup() {
   ////////////////////////// MIDDLE///////////////////////////////////
   pinMode(trigPinM, OUTPUT);         // set trig pin as output
@@ -46,59 +46,27 @@ void loop() {
   dright=detection(trigPinR,echoPinR);
   Serial.print("Distancer: ");
   Serial.println(dright);
-  ///////////////////////////////////////LEFT//////////////////////////////////////////////
-  dleft=detection(trigPinL,echoPinL);
-   Serial.print("Distancel: ");
-  Serial.println(dleft);
 
-  
+
   if(dmiddle>dis){
-    if(dright<range2){
-      Serial.print("Move Left!!");
+    moveForward;
+  }
+  else if(dmiddle<=dis){
+    if(dright<= range){
       moveLeft();
+      delay(del);
     }
-    else if(dleft<range2){
-      Serial.print("Move Right!!");
+    else if(dright>range){
       moveRight();
+      delay(del);
     }
-    else if(dleft>=range2 && dright>=range2){
-      moveForward();
-    }
-    
   }
-  else if(dmiddle<dis&&dright>dleft+range){
-    moveRight();
-  }
-  else if (dmiddle<dis&&dleft>dright+range){
-    moveLeft();
-  }
-
-/*moveForward();
-delay(3000);
-moveRight();
-delay(3000);
-moveLeft();
-delay(3000);*/
-
-/*if(dright<=6&&dleft<=6&&dmiddle>5){
-  moveForward();
-}
-else if(dright>6&&dleft<=6&&dmiddle>5){
-  moveRight();
-}
-else if(dleft>6&&dright<=6&&dmiddle>5){
-  moveLeft();
-}
-else{
-  moveForward();
-}*/
-
 
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 long detection(int trigPin,int echoPin){
 digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);   
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);     // send waves for 10 us
   delayMicroseconds(5);
   digitalWrite(trigPin, LOW);
@@ -129,8 +97,8 @@ void moveBackward(){
   motorL1.setSpeed(100);
   motorR1.setSpeed (100);
   motorL1.run(BACKWARD);
-  motorR1.run(BACKWARD);  
-  
+  motorR1.run(BACKWARD);
+
 }
 void moveRight(){
   motorR.setSpeed(vel);
@@ -141,7 +109,7 @@ void moveRight(){
   motorR1.setSpeed (vel);
   motorL1.run(FORWARD);
   motorR1.run(BACKWARD);
-  
+
 }
 void moveLeft(){
  motorR.setSpeed(vel);
